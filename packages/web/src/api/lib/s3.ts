2e6953c11283b08a3fcc812212c95f3017adc940
Vylanous <dylanjhicks1993@gmail.com>
2026-07-30 23:14:28 -0700

    Update s3.ts

--- packages/web/src/api/lib/s3.ts
+++ packages/web/src/api/lib/s3.ts
@@ -1,7 +1,7 @@
-import { S3Client } from "@aws-sdk/client-s3";
+ import { S3Client } from "@aws-sdk/client-s3";
 
 export const s3 = new S3Client({
-  region: "auto",
+  region: "us-east-1",
   endpoint: process.env.S3_ENDPOINT,
   forcePathStyle: true,
   credentials: {
