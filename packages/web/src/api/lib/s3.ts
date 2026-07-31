d441d0b077e703ce5fa741352786df261ab43bc3
Vylanous <dylanjhicks1993@gmail.com>
2026-07-31 00:33:35 -0700

    Replaced

--- packages/web/src/api/lib/s3.ts
+++ packages/web/src/api/lib/s3.ts
@@ -1,4 +1,4 @@
- import { S3Client } from "@aws-sdk/client-s3";
+import { S3Client } from "@aws-sdk/client-s3";
 
 export const s3 = new S3Client({
   region: "us-east-1",
@@ -8,6 +8,8 @@ export const s3 = new S3Client({
     accessKeyId: process.env.S3_ACCESS_KEY_ID!,
     secretAccessKey: process.env.S3_SECRET_ACCESS_KEY!,
   },
+  // @ts-ignore
+  disableS3ExpressSessionAuth: true,
 });
 
 export const S3_BUCKET = process.env.S3_BUCKET!;
