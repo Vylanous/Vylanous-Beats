83b965fe8aae96485f0e926b6dd4a8973dadbcca
Vylanous <dylanjhicks1993@gmail.com>
2026-08-04 05:52:57 -0700

    Refactor S3 endpoint configuration

--- packages/web/src/api/lib/s3.ts
+++ packages/web/src/api/lib/s3.ts
@@ -2,8 +2,7 @@ import { S3Client } from "@aws-sdk/client-s3";
 
 export const s3 = new S3Client({
   region: "auto",
-  endpoint: `https://${process.env.S3_ENDPOINT}`,
-  forcePathStyle: true,
+  endpoint: process.env.S3_ENDPOINT,
   credentials: {
     accessKeyId: process.env.S3_ACCESS_KEY_ID!,
     secretAccessKey: process.env.S3_SECRET_ACCESS_KEY!,
